a888b2bd2c4daf519c0f8cf733c4550ae9605c7d
Leonardo Fernandes Contrera <leonardo.contrera@bkbank.com.br>
2023-10-19 17:22:43 -0300

    Implementando algumas alterações visuais

--- src/components/TasksForm.jsx
+++ src/components/TasksForm.jsx
@@ -3,33 +3,46 @@ import {useRef, useState} from "react";
 
 function TasksForm({onSubmit}){
     const inputRef = useRef(null);
-    const [text,setText] = useState("");
+    // const [text,setText] = useState("");
 
     async function handleSubmit(event){
         event.preventDefault();
-        
-        const verification = text.trim()
 
-        verification.length > 0 ? await onSubmit(text) : alert("Não é possivel passar uma tarefa vazia")
+        const formData = new FormData(event.target);
 
-        setText("");
-        document.getElementById("text").focus();
+        const texto = formData.get("text");
+
+        const verification = texto.trim();
+
+        verification.length > 0 ? await onSubmit(texto) : alert("Não é possivel passar uma tarefa vazia")       
+
+        event.target.reset()
+
+        // const verification = text.trim()
+
+        // verification.length > 0 ? await onSubmit(text) : alert("Não é possivel passar uma tarefa vazia")
+
+        // setText("");
+   
+        inputRef.current.focus();
     }
 
     return( 
         <form 
             className={styles.Header} 
-            onSubmit={(event) => handleSubmit(event)} >
+            onSubmit={(event) => handleSubmit(event) } >
                 <h1 className={styles.label}>Minhas Tarefas</h1>
             <div className={styles.taskForm}> 
             <input
+                name="text"
+                ref={inputRef}
                 placeholder="Passear com o cachorro"
                 className={styles.input}
                 type="text"
                 id="text"
-                value={text}
-                onChange={(event) => setText(event.target.value)}
-                minLength={5}
+                // value={text}
+                // onChange={(event) => setText(event.target.value)}}
+                minLength={2}
                 required
             />
 
